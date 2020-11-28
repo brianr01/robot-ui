48e55b246c9c3aa718cbcad1f954223c518e84c7
brianr01 <brianr.email1@gmail.com>
2020-11-27 21:05:27 -0500

    Updated from vuetify to bootstrap and added virtual cube display

---
+++ components/rubiksCube.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="cube">
+    <div class="cube-row d-flex justify-content-center">
+      <div class="face d-flex flex-row flex-wrap">
+        <div v-for="(sticker, index) in cubePosition.u" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cube-row d-flex flex-row">
+      <div class="face d-flex flex-row flex-wrap">
+        <div v-for="(sticker, index) in cubePosition.l" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+      <div class="face d-flex flex-row flex-wrap">
+        <div v-for="(sticker, index) in cubePosition.f" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+      <div class="face d-flex flex-row flex-wrap">
+        <div v-for="(sticker, index) in cubePosition.r" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cube-row d-flex justify-content-center">
+      <div class="face d-flex flex-row flex-wrap">
+        <div v-for="(sticker, index) in cubePosition.d" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cube-row d-flex justify-content-center">
+      <div class="face d-flex flex-row-reverse flex-wrap-reverse">
+        <div v-for="(sticker, index) in cubePosition.b" :key="index" class="sticker border border-dark">
+          <div :style="'background-color:' + colorMap[sticker[0]]" class="w-100 h-100">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colorMap: {
+        u: 'white',
+        d: 'yellow',
+        r: 'red',
+        l: 'orange',
+        f: 'green',
+        b: 'blue'
+      },
+    }
+  },
+  props: {
+    cubePosition: Object
+  }
+}
+</script>
+
+<style scoped>
+.cube {
+  width: 540px;
+}
+.face {
+  width: 180px;
+  height: 180px;
+  display: inline-block;
+}
+.sticker {
+  width: 60px;
+  height: 60px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,106 +1,113 @@
 <template>
-  <v-app>
-    <div class="text-center">
-      <v-btn
-        class="ma-2"
-        color="error"
-        @click="powerOff()"
-      >
-        Off
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="success"
-        @click="powerOn()"
-      >
-        On
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('r')"
-      >
-        Right
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('l')"
-      >
-        Left
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('u')"
-      >
-        Up
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('d')"
-      >
-        Down
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('f')"
-      >
-        Front
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="turn('b')"
-      >
-        Back
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="switch_cameras()"
-      >
-        Switch
-      </v-btn>
-      <v-btn
-        class="ma-2"
-        color="primary"
-        @click="scramble()"
-      >
-        Scramble
-      </v-btn>
-      <div style="float:left"  width="40%">
-        <h3>Top</h3>
-        <img src="http://127.0.0.1:5000/video_feed_1">
+    <div class="text-center" style="background-color:black;">
+      <div class="d-flex flex-column">
+        <div>
+          <h3>Top</h3>
+          <img src="http://127.0.0.1:5000/video_feed_1">
+        </div>
+        <div>
+          <h3>Bottom</h3>
+          <img src="http://127.0.0.1:5000/video_feed_2">
+        </div>
       </div>
-      <div style="float:right" width="40%">
-        <h3>Bottom</h3>
-        <img src="http://127.0.0.1:5000/video_feed_2">
+      <div class="m-3 d-flex justify-content-center">
+        <rubiksCube :cubePosition="cubePosition">
+        </rubiksCube>
+      </div>
+      <div>
+        <button
+          type="button"
+          class="btn btn-danger"
+          @click="powerOff()"
+        >
+          Off
+        </button>
+        <button
+          type="button"
+          class="btn btn-success"
+          @click="powerOn()"
+        >
+          On
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('r')"
+        >
+          Right
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('l')"
+        >
+          Left
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('u')"
+        >
+          Up
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('d')"
+        >
+          Down
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('f')"
+        >
+          Front
+        </button>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="turn('b')"
+        >
+          Back
+        </button>
+        <button
+          type="button"
+          class="btn btn-info"
+          @click="switch_cameras()"
+        >
+          Switch
+        </button>
+        <button
+          type="button"
+          class="btn btn-warning"
+          @click="scramble()"
+        >
+          Scramble
+        </button>
       </div>
     </div>
-  </v-app>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import rubiksCube from '~/components/rubiksCube.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    rubiksCube
   },
   data() {
     return {
       test: '',
+      cubePosition: {}
     }
   },
   async created() {
     const axios = require('axios');
 
-    console.log(this.test)
+    this.cubePosition = (await this.$axios.get('/api/get_cube_position')).data
   },
   methods: {
     async powerOff() {
@@ -111,6 +118,8 @@ export default {
     },
     async turn(side) {
       await this.$axios.get(`/api/turn?side=${side}&direction=c`)
+
+      this.cubePosition = (await this.$axios.get('/api/get_cube_position')).data
     },
     async switch_cameras() {
       await this.$axios.get(`/api/switch_video_feeds`)
